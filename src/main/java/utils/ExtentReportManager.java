6bccd88af8c6223d2f9dfe9f5151d732105780c5
C-Chaithra <chaithrac2358@gmail.com>
2025-06-11 22:54:58 +0530

    Added Thread safe for parallel execution

--- src/main/java/utils/ExtentReportManager.java
+++ src/main/java/utils/ExtentReportManager.java
@@ -8,7 +8,7 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-
+import org.testng.ITestContext;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
@@ -16,15 +16,16 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 public class ExtentReportManager {
 
 	private static ExtentReports extent;
+	private static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
 	private static ExtentTest test;
 	public static String reportPath;
 
 	public static ExtentReports getReportInstance() {
 
-		if(extent==null) {
+		if (extent == null) {
 
 			String timestamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(new Date());
-			reportPath = "reports/ExtentReport_"+timestamp+".html";
+			reportPath = "reports/ExtentReport_" + timestamp + ".html";
 			ExtentSparkReporter reporter = new ExtentSparkReporter(reportPath);
 
 			reporter.config().setDocumentTitle("Selenium Automation Test Report");
@@ -37,23 +38,34 @@ public class ExtentReportManager {
 		return extent;
 	}
 
-	public static ExtentTest createTest(String testName) {
+	public static ExtentTest createTest(String testName, ITestContext context) {
 
-		test = getReportInstance().createTest(testName);
+		String testNgTestName = testName + " - " + context.getName();
+		test = getReportInstance().createTest(testNgTestName);
+		extentTest.set(test);
 		return test;
+	}
+
+	public static ExtentTest getTest() {
+		return extentTest.get();
+	}
+
+	public static void flushReport() {
+		if (extent != null) {
+			extent.flush();
+		}
 	}
 
 	public static String captureScreenshot(WebDriver driver, String screenshotName) {
 		try {
 
 			File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-			String path= System.getProperty("user.dir")+ "/screenshots/"+screenshotName+".png";
-			System.out.println("Path for screenshot is : "+path);
+			String path = System.getProperty("user.dir") + "/screenshots/" + screenshotName + ".png";
+			System.out.println("Path for screenshot is : " + path);
 			FileUtils.copyFile(src, new File(path));
 			return path;
 
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
